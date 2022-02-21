cac342c0f0edcb2e6e4eb67517f6f108b0d88748
devmaffi <devmaffi550@gmail.com>
2022-02-21 21:24:46 +0200

    Init swiper for cards in new section
    Module Swiper was removed cause its usability.
    
    Added swiper classes to new pug section.
    
    Modified card pug mixin to word with slide cards.
    
    Added new eslint rules.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,9 @@
 // Modules
 
 // modules
-import { handleMenu, handleScroll, initSwiper } from '@modules'
+import Swiper, { Navigation } from 'swiper'
+
+import { handleMenu, handleScroll } from '@modules'
 
 // styles
 import 'swiper/css'
@@ -13,6 +15,27 @@ import '@sass/styles'
 handleMenu()
 handleScroll()
 
-// Swiper
+// Swipers init
 
-initSwiper()
+// testimonial swiper
+new Swiper('.testimonial-swiper', {
+  modules: [Navigation],
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+// new swiper
+new Swiper('.new-swiper', {
+  modules: [Navigation],
+  spaceBetween: 24,
+  loop: true,
+  breakpoints: {
+    576: { slidesPerView: 2 },
+    768: { slidesPerView: 3 },
+    1024: { slidesPerView: 4 },
+  },
+})
--- src/assets/js/modules/index.js
+++ src/assets/js/modules/index.js
@@ -1,3 +1,2 @@
 export { default as handleMenu } from './menuHandler'
 export { default as handleScroll } from './scrollHandler'
-export { default as initSwiper } from './swiper'
