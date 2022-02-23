c2f32ff2712bb84d2e2e9692ed59cba3d2069096
devmaffi <devmaffi550@gmail.com>
2022-02-23 22:09:04 +0200

    Handle scroll sections active links
    Was designed functionality, which implements nav menu active links
    handling.
    
    Added new sass module active-link intended to style active nav links.
    
    Removed useless ids from sections which not included in the nav menu.
    
    Added new rules to eslintrc.

--- src/assets/js/modules/scrollHandler.js
+++ src/assets/js/modules/scrollHandler.js
@@ -1,5 +1,6 @@
 // Change background header
 
+// this refers to window
 function scrollHeader() {
   const header = document.querySelector('#header')
 
@@ -8,8 +9,32 @@ function scrollHeader() {
   else header.classList.remove('scroll-header')
 }
 
+function handleActiveLink(section, scrollY) {
+  const sectionHeight = section.offsetHeight
+  const sectionTop = section.offsetTop - 58
+  const sectionId = section.getAttribute('id')
+  const activeSection = document.querySelector(
+    `.nav__menu a[href*=${sectionId}]`
+  )
+
+  // when the scroll is within the section, add the active-link class to the corresponding nav link in the nav menu, otherwise, remove it
+  if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+    return activeSection.classList.add('active-link')
+
+  return activeSection.classList.remove('active-link')
+}
+
+// this refers to window
+function scrollActive() {
+  const sections = document.querySelectorAll('section[id]')
+  const scrollY = this.pageYOffset
+
+  sections.forEach(section => handleActiveLink(section, scrollY))
+}
+
 function handleScroll() {
   window.addEventListener('scroll', scrollHeader)
+  window.addEventListener('scroll', scrollActive)
 }
 
 // Exports
